3e99b425e41c409678753eb232f012f4c89dc15d
Xc9crazy <131987452+Xc9crazy@users.noreply.github.com>
2023-12-08 16:32:40 +0900

    commit
    a

---
+++ components/contact.js
@@ -0,0 +1,8 @@
+import styles from 'styles/contact.module.css'
+
+export default function Contact() {
+    <div className={styles.stack}>
+        <h3 className={styles.heading}>Contact</h3>
+        <address>cube@web.mail.address</address>
+    </div>
+}
