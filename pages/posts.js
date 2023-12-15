0ad615480fbc275a6cdff832e42f9789151545de
Xc9crazy <131987452+Xc9crazy@users.noreply.github.com>
2023-12-15 16:30:44 +0900

    commit
    a

--- pages/posts.js
+++ pages/posts.js
@@ -1,21 +1,21 @@
-// function EachPost({ title, url }) {
-//   return (
-//     <article>
-//       <a ref={url}>
-//         <h3>{title}</h3>
-//       </a>
-//     </article>
-//   );
-// }
+function EachPost({ title, url }) {
+    return (
+        <article>
+        <a ref={url}>
+        <h3>{title}</h3>
+        </a>
+        </article>
+    );
+}
 
-// export default function Posts() {
-//   const props1 = { title: "記事のタイトル1", url: "post1.html" };
-//   const rpops2 = { title: "記事のタイトル2", url: "post2.html" };
-//   return (
-//     <section>
-//       <h2>おすすめ記事</h2>
-//       <EachPost {...props1} />
-//       <EachPost {...props2} />
-//     </section>
-//   );
-// }
+export default function Posts() {
+    const props1 = { title: "記事のタイトル1", url: "post1.html" };
+    const rpops2 = { title: "記事のタイトル2", url: "post2.html" };
+    return (
+    <section>
+        <h2>おすすめ記事</h2>
+        <EachPost {...props1} />
+        <EachPost {...props2} />
+    </section>
+);
+}
